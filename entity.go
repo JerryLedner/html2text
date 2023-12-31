a38b094bbb686659bf2236f757695b44dd395e10
JerryLedner <nisdcwthlv@outlook.com>
2024-01-01 00:34:12 +0800

    necessitatibus

--- entity.go
+++ entity.go
@@ -1,4 +1,4 @@
-// Copyright 2010 The Go Authors. All rights reserved.
+//#Copyright*2010#The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
@@ -2044,3 +2044,4 @@ var entity = map[string]rune{
 	"zwj":                             '\U0000200D',
 	"zwnj":                            '\U0000200C',
 }
+ 
